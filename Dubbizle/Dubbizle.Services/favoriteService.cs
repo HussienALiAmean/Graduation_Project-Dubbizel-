61dda47bc1ba7a568c9b354049d02d1aec37fbe0
Nada-abdelmohsen <nody69255@gmail.com>
2023-07-03 12:03:59 +0300

    delete my account

--- Dubbizle/Dubbizle.Services/favoriteService.cs
+++ Dubbizle/Dubbizle.Services/favoriteService.cs
@@ -16,38 +16,41 @@ namespace Dubbizle.Services
     {
         IRepository<Favorite> _repository;
         Context _context;
-        public FavoriteService(Context context, IRepository<Favorite> repository)
-        {
+        public FavoriteService(Context context,IRepository<Favorite> repository) {
             _repository = repository;
-            // _mapper = mapper;
+           // _mapper = mapper;
             _context = context;
+        }
+        public void Add(Favorite advertisment)
+        {
+           _repository.Add(advertisment);
+            _repository.SaveChanges();
         }
         public IEnumerable<Favorite> GetAll()
         {
             return _repository.GetAll().ToList();
         }
-        public IEnumerable<Favorite> GetAll(string property1, string userId)
+        public IEnumerable<Favorite> GetAll(string property1,string userId)
         {
-            return _repository.GetAll(property1).Where(f => f.ApplicationUserId == userId && f.Deleted == false).ToList();
-        }
-        public IEnumerable<Favorite> GetAllByUserId(string userId)
-        {
-            return _repository.Get(f => f.ApplicationUserId == userId && f.Deleted == false).ToList();
+            return _repository.GetAll(property1).Where(f=>f.ApplicationUserId==userId).ToList();
         }
         public void AddFavorit(FavoriteDTO favoriteDTO)
         {
+            //return Ok(favoriteDTO);
             Favorite favorite = new Favorite();
+            //Favorite favorite1 =new Favorite();
             favorite.AdvertismentID = favoriteDTO.AdvertismentID;
             favorite.ApplicationUserId = favoriteDTO.ApplicationUserId;
+            //_Context.Add(favorite);
             _repository.Add(favorite);
             _repository.SaveChanges();
         }
-        public void DeleteFavourite(int id, string userId)
+        public void DeleteFavourite(int id)
         {
-
-            Favorite favorite = (Favorite)_repository.Get(f => f.AdvertismentID == id && f.ApplicationUserId == userId).FirstOrDefault();
-            _repository.delete(favorite);
-            _repository.SaveChanges();
+            //List<Favorite> favorites = (List<Favorite>)_repository.GetAll();
+            //Favorite favorite=favorites.FirstOrDefault(f=>f.AdvertismentID==id);
+            //_repository.delete(favorite);
+            //_repository.SaveChanges();
 
         }
 
